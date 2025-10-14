8e8eba9b530051dedeb3e1a1aabe6b8de6a7df65
ohu-star <audtj0703@naver.com>
2025-10-15 04:01:21 +0900

    🎉 [chore]#0 initial setting

---
+++ src/main/java/profect/group1/goormdotcom/GoormdotcomApplication.java
@@ -0,0 +1,13 @@
+package profect.group1.goormdotcom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoormdotcomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoormdotcomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/profect/group1/goormdotcom/GoormdotcomApplicationTests.java
@@ -0,0 +1,13 @@
+package profect.group1.goormdotcom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoormdotcomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
