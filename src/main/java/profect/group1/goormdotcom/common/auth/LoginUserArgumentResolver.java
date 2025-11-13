8d16e6fc5463f0ab65c29098fdbc87934af87067
ohu-star <audtj0703@naver.com>
2025-11-13 19:54:01 +0900

    🔧 [config]#11 LoginUser(커스텀 어노테이션) 추가

---
+++ src/main/java/profect/group1/goormdotcom/common/auth/LoginUserArgumentResolver.java
@@ -0,0 +1,46 @@
+package profect.group1.goormdotcom.common.auth;
+
+
+import java.util.UUID;
+
+import org.springframework.core.MethodParameter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+import profect.group1.goormdotcom.common.dto.UserContext;
+
+@Component
+public class LoginUserArgumentResolver implements HandlerMethodArgumentResolver {
+
+    @Override
+    public boolean supportsParameter(final MethodParameter parameter) {
+        return parameter.hasParameterAnnotation(LoginUser.class)
+                && parameter.getParameterType().equals(UUID.class);
+    }
+
+    @Override
+    public Object resolveArgument(final MethodParameter parameter,
+                                  final ModelAndViewContainer mavContainer,
+                                  final NativeWebRequest webRequest,
+                                  final WebDataBinderFactory binderFactory)
+            throws Exception {
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null || authentication.getPrincipal() == null) {
+            throw new IllegalStateException("No authentication found");
+        }
+
+        Object principal = authentication.getPrincipal();
+
+        if (!(principal instanceof UserContext userContext)) {
+            throw new IllegalStateException(
+                    "Unexpected principal type: " + principal.getClass().getName());
+        }
+
+        return userContext.getId();
+    }
+}
